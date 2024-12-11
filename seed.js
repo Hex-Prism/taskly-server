e8a2f1e89221f748fa2aee164b72da82c9939e41
Hex Prism <146946734+Hex-Prism@users.noreply.github.com>
2024-12-11 18:44:19 +0500

    Add files via upload

--- seed.js
+++ seed.js
@@ -22,40 +22,38 @@ const users = [
 
 const tasks = [
     {
-        name: 'Прочитать книгу "Atomic Habits"',
-        description: 'Закончить чтение книги "Atomic Habits" Джеймса Клира',
-        priority: 'не срочно',
+        name: 'Read biik "Atomic Habits"',
+        description: 'Done to reaad "Atomic Habits"',
+        priority: 'not emergency',
         due: new Date().toISOString(),
-        status: 'открыта',
+        status: 'open',
         createdAt: new Date().toISOString(),
         updatedAt: new Date().toISOString(),
     },
     {
-        name: 'Изучить стек MERN',
-        description: 'Изучить стек MERN и создать с его помощью полнофункциональное приложение',priority: 'срочно',
+        name: 'Study MERN',
+        description: 'Study MERN',priority: 'emergency',
         due: new Date().toISOString(),
-        status: 'открыта',
+        status: 'open',
         createdAt: new Date().toISOString(),
         updatedAt: new Date().toISOString(),
     },
 ];
 
 try {
-    // Добавление пользователей
     let collection = db.collection('users');
-    console.log('[seed]', 'Добавляем пользователей...');
+    console.log('[seed]', 'Adding users...');
     const result = await collection.insertMany(users);
     console.log(result.insertedIds);
-    console.log('[seed]', 'Добавление пользователей завершено');
-    // Добавление задач
+    console.log('[seed]', 'Adding users is done');
     tasks[0].owner = result.insertedIds[0];
     tasks[1].owner = result.insertedIds[1];
     collection = db.collection('tasks');
-    console.log('[seed]', 'Добавляем задачи...');
+    console.log('[seed]', 'Adding tasks...');
     await collection.insertMany(tasks);
-    console.log('[seed]', 'Добавление задач завершено');
-    console.log('[seed]', 'Все завершено');
+    console.log('[seed]', 'Adding tasks is done');
+    console.log('[seed]', 'All is done');
 } catch (error) {
-    console.log('[seed]', 'Ошибка: ', error);
+    console.log('[seed]', 'Error: ', error);
 }
 process.exit();
